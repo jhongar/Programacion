abc7a68720d3c2811a97e55ae8fac6203cb17abe
jhongar <jotica_94@hotmail.com>
2020-09-12 01:48:35 -0500

    solucion

--- src/palabraPalindrome/Palindrome.java
+++ src/palabraPalindrome/Palindrome.java
@@ -2,9 +2,28 @@ package palabraPalindrome;
 
 public class Palindrome {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		String palabra = "recnocer";
+		
+		System.out.println("la palabra "+ palabra +" es palindrome");
+		
 
+	}
+	
+	public boolean getPalindromo(String palabra, String i, String j, String pala) 
+	{
+		if(palabra.length()==pala) 
+		{
+			if(palabra.charAt(i)==palabra.charAt(j)) 
+			{
+				pala=palabra;
+				getPalindromo(palabra, i+1, j, pala);
+				return true;
+			}
+			
+		}
+		return false;
 	}
 
 }
